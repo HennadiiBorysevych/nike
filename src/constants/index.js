e53ff0d9991368f16d5fc94b11d4efa385ae17f5
Hennadii Borysevych <genuch3@gmail.com>
2023-09-07 20:46:01 +0200

    feat:Services section

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,14 @@ import {
   thumbnailShoe2,
   thumbnailShoe3,
 } from "../assets/images";
-
+import {
+  facebook,
+  instagram,
+  shieldTick,
+  support,
+  truckFast,
+  twitter,
+} from "../assets/icons";
 export const navLinks = [
   { href: "#home", label: "Home" },
   { href: "#about-us", label: "About Us" },
@@ -42,23 +49,41 @@ export const shoes = [
 ];
 export const products = [
   {
-      imgURL: shoe4,
-      name: "Nike Air Jordan-01",
-      price: "$200.20",
+    imgURL: shoe4,
+    name: "Nike Air Jordan-01",
+    price: "$200.20",
   },
   {
-      imgURL: shoe5,
-      name: "Nike Air Jordan-10",
-      price: "$210.20",
+    imgURL: shoe5,
+    name: "Nike Air Jordan-10",
+    price: "$210.20",
   },
   {
-      imgURL: shoe6,
-      name: "Nike Air Jordan-100",
-      price: "$220.20",
+    imgURL: shoe6,
+    name: "Nike Air Jordan-100",
+    price: "$220.20",
   },
   {
-      imgURL: shoe7,
-      name: "Nike Air Jordan-001",
-      price: "$230.20",
+    imgURL: shoe7,
+    name: "Nike Air Jordan-001",
+    price: "$230.20",
+  },
+];
+export const services = [
+  {
+    imgURL: truckFast,
+    label: "Free shipping",
+    subtext: "Enjoy seamless shopping with our complimentary shipping service.",
+  },
+  {
+    imgURL: shieldTick,
+    label: "Secure Payment",
+    subtext:
+      "Experience worry-free transactions with our secure payment options.",
+  },
+  {
+    imgURL: support,
+    label: "Love to help you",
+    subtext: "Our dedicated team is here to assist you every step of the way.",
   },
 ];
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,7 +1,13 @@
+import { offer } from "../assets/images";
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
-  )
-}
+    <section className="flex items-center justify-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} alt="offer" width={773} height={687} className="object-contain w-full"  />
+      </div>
+    </section>
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
